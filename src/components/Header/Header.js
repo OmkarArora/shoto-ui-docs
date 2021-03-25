aadc3b0a64869e948f9215d9ed3c2d02163edf89
Omkar Arora <aroraomkar@gmail.com>
2021-03-25 12:04:56 +0530

    base app setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,3 @@
+export const Header = () => {
+	return <div style={{border: "1px solid red"}}>HEADER</div>
+}
---
+++ src/components/Sidenav/Sidenav.js
@@ -0,0 +1,10 @@
+import "./sidenav.css";
+
+export const Sidenav = () => {
+	return <nav className="sidenav">
+		<ul>
+			<li><button>Getting Started</button></li>
+			<li><button>Components</button></li>
+		</ul>
+	</nav>
+}
